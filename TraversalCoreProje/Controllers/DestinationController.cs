c7e2814ed544e710d50ff9a7489e99d5a9ee7e1a
Toprak Tüzün <topraktuzun17@hotmail.com>
2022-08-06 23:24:20 +0300

    add Destination and DestinationDetails page

---
+++ TraversalCoreProje/Controllers/DestinationController.cs
@@ -0,0 +1,33 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class DestinationController : Controller
+    {
+        DestinationManager destinationManager = new DestinationManager(new EfDestinationDal());
+        public IActionResult Index()
+        {
+            var values = destinationManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult DestinationDetails(int id) 
+        {
+            var values=destinationManager.TGetById(id);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult DestinationDetails(DestinationController p) 
+        {
+            return View();
+        }
+    }
+}
