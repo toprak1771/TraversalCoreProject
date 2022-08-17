7b30088cc8ce66c83c1ddd953ee20bf8b76ec41d
Toprak Tüzün <topraktuzun17@hotmail.com>
2022-08-17 17:10:39 +0300

    add addComment

---
+++ TraversalCoreProje/Controllers/CommentController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Controllers
+{
+    public class CommentController : Controller
+    {
+        CommentManager commentManager = new CommentManager(new EfCommentDal());
+
+        [HttpGet]
+        public PartialViewResult AddComment() 
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public IActionResult AddComment(Comment comment)
+        {
+
+            comment.CommnetDate = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+            comment.CommentState = true;
+            commentManager.TAdd(comment);
+            return RedirectToAction("Index", "Destination");
+
+        }
+
+
+    }
+}
