bd03f7e0eba59c429ff4ca396bcd7aed55c6bfe3
Toprak Tüzün <topraktuzun17@hotmail.com>
2022-08-22 18:15:19 +0300

    add SignIn,SignUp pages and CustomIdentityValidator class

---
+++ TraversalCoreProje/Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TraversalCoreProje.Models;
+
+namespace TraversalCoreProje.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+        private readonly UserManager<AppUser> _userManager;
+
+        public LoginController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserRegisterViewModel registerModel)
+        {
+            AppUser appUser = new AppUser()
+            {
+                Name=registerModel.Name,
+                Surname=registerModel.Surname,
+                Email=registerModel.Mail,
+                UserName=registerModel.UserName  
+            };
+            if(registerModel.Password == registerModel.ConfirmPassword) 
+            {
+                var result = await _userManager.CreateAsync(appUser, registerModel.Password);
+
+                if (result.Succeeded) 
+                {
+                    return RedirectToAction("SignIn");
+                }
+                else 
+                {
+                    foreach (var item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+            return View(registerModel);
+        }
+
+        [HttpGet]
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
+
---
+++ TraversalCoreProje/Models/CustomIdentityValidator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Models
+{
+    public class CustomIdentityValidator : IdentityErrorDescriber
+    {
+        public override IdentityError PasswordTooShort(int length)
+        {
+            return new IdentityError()
+            {
+                Code= "PasswordTooShort",
+                Description=$"Parola minimum {length} karakter olmalıdır."
+            };
+        }
+
+        public override IdentityError PasswordRequiresLower()
+        {
+            return new IdentityError()
+            {
+                Code= "PasswordRequiresLower",
+                Description="Parola en az bir küçük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresUpper()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresLower",
+                Description = "Parola en az bir büyük harf içermelidir."
+            };
+        }
+
+        public override IdentityError PasswordRequiresNonAlphanumeric()
+        {
+            return new IdentityError()
+            {
+                Code = "PasswordRequiresNonAlphanumeric",
+                Description = "Parola en az bir sembol içermelidir."
+            };
+        }
+    }
+}
